578ee14acc7ac81df0a4118e2a65431754f24599
unknown <indraradian@gmail.com>
2018-12-08 18:13:45 +0700

    heloo

---
+++ app.js
@@ -0,0 +1,23 @@
+
+const express = require("express")
+const app = express()
+
+var statusLampu = 0 ; //variabelglobal
+var statusLampu2 = 0
+
+app.listen(3000,function(){
+    console.log("server jalan")
+})
+
+app.get("/:lampu/:lampu2",function(req,res){
+    statusLampu = req.params.lampu, //mengambil variabel dari url
+    statuslampu2 = req.params.lampu2
+    res.redirect("/") //untuk mengembalikan / pada url
+})
+
+app.get("/",function(req,res){
+    res.send({
+        lampu:statusLampu,
+        lampu2:statusLampu2
+    })
+})
